e57131ac3e70cc65d6fbff2be0c5910fa33abacb
Tanujeet <singhtanujeet@gmail.com>
2025-10-30 21:17:58 +0530

    Added:Fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${SchibstedGrotesk.variable} ${MartianMono.variable} antialiased`}
       >
         {children}
       </body>
